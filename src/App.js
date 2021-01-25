36d6b199c049d63789093d0db35c5140436521f8
mdestafadilah <desta.08b@gmail.com>
2021-01-25 15:01:56 +0700

    Login interface with auth0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import LoginButton from "./components/LoginButton";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <LoginButton />
     </div>
   );
 }
