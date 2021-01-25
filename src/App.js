93c97842d48a85457109469a21610020073a681e
mdestafadilah <desta.08b@gmail.com>
2021-01-25 15:34:02 +0700

    Section 01 Done. Cek Sudah Login atau Logout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import LoginButton from "./components/LoginButton";
+import AuthButton from "./components/AuthButton";
 import Profile from "./components/Profile";
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <LoginButton />
+      <AuthButton />
       <Profile />
     </div>
   );
